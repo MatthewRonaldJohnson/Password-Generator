213cb14cee63ac244ecf0104713a93e71f958edf
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-02-24 21:39:35 -0600

    seperated char arrays so users can select types

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Assignment Code
-var possibleChars = [
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
+//Arrays used to generate list of acceptable characters 
+var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var undercaseArray = [
   "a",
   "b",
   "c",
@@ -35,9 +26,38 @@ var possibleChars = [
   "x",
   "y",
   "z",
+];
+var uppercaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specArray = [
   "",
   "!",
-  "\"",
+  '"',
   "#",
   "$",
   "%",
@@ -70,11 +90,40 @@ var possibleChars = [
   "~",
 ];
 
-var passwordLength = 10;
+//Variables that hold boolean values based on user inputs for what chars list to use
+var numBool = false;
+var underBool = true;
+var upperBool = true;
+var specBool = false;
 
-var generatedPW = [0]
+//Array that will contain the arrays the user wants to get chars from
+var accetedArrays = []
 
-var random = 0;
+//Array that will contain acceptable characters for password
+var charList= []
+
+//Variable that takes user input to define length of password 
+var passwordLength = 15;
+
+//Array that will hold generated password
+var generatedPW = [0];
+
+//Set of if statments that add selected arrays into acctped arrays list
+if (numBool) {
+  accetedArrays.push(numberArray);
+}
+if (underBool) {
+  accetedArrays.push(undercaseArray);
+}
+if (upperBool) {
+  accetedArrays.push(uppercaseArray);
+}
+if (specBool) {
+  accetedArrays.push(specArray);
+}
+
+//Fills out charList array 
+charList = accetedArrays.flat();
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -82,22 +131,18 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-for (i=0; i < passwordLength; i++){
-  random = getRandomInt(0, 68);
-  generatedPW[i] = possibleChars[random];
+for (i = 0; i < passwordLength; i++) {
+  random = getRandomInt(0, charList.length);
+  generatedPW[i] = charList[random];
 }
 
-console.log(generatedPW)
+console.log(generatedPW);
 
-for (y=0; y < generatedPW.length; y++){
+for (y = 0; y < generatedPW.length; y++) {
   console.log(generatedPW[y]);
 }
 
-
-
-
-
-// starter code 
+// starter code
 // var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
