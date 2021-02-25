1d9b46164a281a9179d4c92df2d85adde63871bf
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-02-25 14:08:47 -0600

    moved code inside function

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Homework 3: Password Generator made with Javascript
 
 Webpage is deployed [here](https://matthewronaldjohnson.github.io/Password-Generator/). 
+
+## Resources 
+
+MDN Page on Math.random(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+MDN Page on Array.protoype.flat(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+
+Javascript.info page on alert, prompt, and confirm: https://javascript.info/alert-prompt-confirm
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Arrays used to generate list of acceptable characters
-var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Defining variables globally
+var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //these four arrays hold the different master list of possible characters for the password
 var undercaseArray = [
   "a",
   "b",
@@ -89,60 +89,62 @@ var specArray = [
   "}",
   "~",
 ];
-
-//Variables that hold boolean values based on user inputs for what chars list to use
-var numBool = true;
+var passwordLength = 8; //length of password that user will input, defaults to 8
+var numBool = true; //these variables hold the user input on if to use a certain set of characters or not
 var underBool = true;
 var upperBool = true;
 var specBool = true;
-
-//Array that will contain the arrays the user wants to get chars from
-var accetedArrays = [];
-
-//Variable that takes user input to define length of password
-//needs to be an interger between 8 and 128
-var passwordLength = 20;
-
-
-//Checks that user entered passwordLength is between 8 & 128 and is an interger 
-if (passwordLength >= 8 && passwordLength <= 128 && Number.isInteger(passwordLength)){
-  console.log("true");
-}
-
-//Array that will hold generated password
-var generatedPW = [0];
-
-//Set of if statments that add selected arrays into acctped arrays list
-if (numBool) {
-  accetedArrays.push(numberArray);
-}
-if (underBool) {
-  accetedArrays.push(undercaseArray);
-}
-if (upperBool) {
-  accetedArrays.push(uppercaseArray);
-}
-if (specBool) {
-  accetedArrays.push(specArray);
-}
+var accetedArrays = []; //Array that will contain the arrays the user wants to get chars from
+var generatedPW = []; //Array that will hold generated password
 
 //function that generates a random integer between 0 and max value (inclusive of 0 and exclusive of max)
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-for (i = 0; i < passwordLength; i++) {
-  random = getRandomInt(accetedArrays.flat().length);
-  generatedPW[i] = accetedArrays.flat()[random];
+function generatePW () {
+  document.getElementById("password").innerHTML = ""; //this line clears out the previously generated password
+  passwordLength = prompt ("Enter your desired password length(must be an interger between 8 and 128", passwordLength);
+  if (!(passwordLength >= 8 && passwordLength <= 128)){
+    alert("Password Length must be an interger between 8 and 128!");
+    return;
+  }
+  numBool = confirm("Do you want your password to contain numbers?");
+  underBool = confirm("Do you want your password to contain lowercase letters?");
+  upperBool = confirm("Do you want your password to contain uppercase letters?");
+  specBool = confirm("Do you want your password to contain special characters");
+  if (numBool) {
+    accetedArrays.push(numberArray);
+  }
+  if (underBool) {
+    accetedArrays.push(undercaseArray);
+  }
+  if (upperBool) {
+    accetedArrays.push(uppercaseArray);
+  }
+  if (specBool) {
+    accetedArrays.push(specArray);
+  }
+  if (accetedArrays === undefined || accetedArrays.length === 0){
+    alert("You must select at least one of the character list");
+    return;
+  }
+  for (i = 0; i < passwordLength; i++) {
+    random = getRandomInt(accetedArrays.flat().length);
+    generatedPW[i] = accetedArrays.flat()[random];
+  }
+  //loop that prints out PW in the #password textarea
+  for (i = 0; i < generatedPW.length; i++) {
+  console.log(generatedPW[i]);
+  document.getElementById("password").append(generatedPW[i]);
+  }
 }
 
-console.log(generatedPW);
+generatePW();
 
-for (y = 0; y < generatedPW.length; y++) {
-  console.log(generatedPW[y]);
-}
 
-// starter code
+
+// //starter code
 // var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
